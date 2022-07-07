fbb522492e201117c8f77755b6999204adfd6e69
Abhishek Bhosale <abhisheksachinbhosale@gmail.com>
2022-07-07 10:40:46 +0530

    some changes

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from streamlit_lottie import st_lottie
 import yfinance as yf
 import pandas as pd
 import plotly.graph_objects as go
-import BBands as bb
+#import MovingAverage as mova
 from backtesting import Strategy
 from backtesting import Backtest
 import numpy as np
@@ -14,41 +14,6 @@ from backtesting import Backtest, Strategy
 from backtesting.lib import crossover
 from datetime import datetime as dt
 
-
-fig = None
-
-
-# --- MA CROSSOVER STRATEGY ---
-def macross():
-
-    class SMACross(Strategy):
-        n1 = 50
-        n2 = 200
-
-        def init(self):
-            close = self.data.Close
-            self.sma1 = self.I(ta.trend.ema_indicator, pd.Series(close), self.n1)
-            self.sma2 = self.I(ta.trend.ema_indicator, pd.Series(close), self.n2)
-
-        def next(self):
-            if crossover(self.sma1, self.sma2):
-                self.buy()
-            elif crossover(self.sma2, self.sma1):
-                self.sell()
-
-    df = yf.download(ticker, start=dt.today() - pd.Timedelta(days=3000))
-    df.dropna(inplace=True)
-    bt = Backtest(df, SMACross, cash=10000, commission=0.00, exclusive_orders=True)
-    stat = bt.run()
-    st.write('Strategy Returns [%] : ' + str(dict(stat)['Return [%]']))
-    st.write('Buy & Hold Return [%] : ' + str(dict(stat)['Buy & Hold Return [%]']))
-    st.write('Win Rate [%] : ' + str(dict(stat)['Win Rate [%]']))
-    df['MA1'] = pta.ema(df.Close, length=50)
-    df['MA2'] = pta.ema(df.Close, length=200)
-    df.dropna(inplace=True)
-    df['Signal'] = 0.0
-    df['Signal'] = np.where(df['MA1'] > df['MA2'], 1.0, 0.0)
-    df['Position'] = df['Signal'].diff()
 
 # --- FUNCTION TO LOAD LOTTIE FILES ---
 def load_lottieurl(url: str):
@@ -105,9 +70,107 @@ with m:
 with r:
     st_lottie(lottie_anim1, key="anim1", width=410, height=310)
 
+
+# --- MA Strategy ---
+df = yf.download(ticker, start=dt.today() - pd.Timedelta(days=3000))
+df = df[df['Volume'] != 0]
+df.isna().sum()
+
+df["EMA"] = pta.ema(df.Close, length=100)
+df['ATR'] = pta.atr(high=df.High, low=df.Low, close=df.Close, length=14)
+
+def EMASig(df1, l, backcandles):
+    sigup=2
+    sigdn=1
+    for i in range(l-backcandles, l+1):
+        if df1.Low[i]<=df1.EMA[i]:
+            sigup = 0
+        if df1.High[i]>=df1.EMA[i]:
+            sigdn = 0
+    if sigup:
+        return sigup
+    elif sigdn:
+        return sigdn
+    else:
+        return 0
+
+dfpl = df[100:1000]
+fig = go.Figure(data=[go.Candlestick(x=dfpl.index,
+                                     open=dfpl['Open'],
+                                     high=dfpl['High'],
+                                     low=dfpl['Low'],
+                                     close=dfpl['Close']),
+                      go.Scatter(x=dfpl.index, y=dfpl.EMA, line=dict(color='orange', width=1), name="EMA")])
+
+EMABackCandles = 20
+EMASignal = [0] * len(df)
+for row in range(EMABackCandles, len(df)):
+    EMASignal[row] = EMASig(df, row, EMABackCandles)
+df['EMASignal'] = EMASignal
+df.dropna(inplace=True)
+
+HLBackCandles = 8
+df['mins'] = df['Low'].rolling(window=HLBackCandles).min()
+df['maxs'] = df['High'].rolling(window=HLBackCandles).max()
+def HLSig(x):
+    if x.EMASignal == 1 and x.High >= float(int(x.maxs)):
+        return 1
+    if x.EMASignal == 2 and x.Low <= x.mins:
+        return 2
+    else:
+        return 0
+df["HLSignal"] = df.apply(HLSig, axis=1)
+df[df['HLSignal'] == 1]
+
+def pointpos(x):
+    if x['HLSignal'] == 1:
+        return x['High']+1e-3
+    elif x['HLSignal'] == 2:
+        return x['Low']-1e-3
+    else:
+        return np.nan
+df['pointpos'] = df.apply(lambda r: pointpos(r), axis=1)
+
+def SIGNAL():
+    return df.HLSignal
+
+class MyStrat(Strategy):
+    atr_f = 1.
+    SLTPR = 1.5
+    def init(self):
+        super().init()
+        self.signal1 = self.I(SIGNAL)
+
+    def next(self):
+        super().next()
+        if self.signal1==2:
+            #sl1 = self.data.Close[-1] - 750e-4 #EURUSD
+            #tp1 = self.data.Close[-1] + 600e-4
+            #sl1 = self.data.Close[-1] - 30 #ETHUSD
+            #tp1 = self.data.Close[-1] + 40
+            sl1 = self.data.Close[-1] - self.data.ATR[-1]/self.atr_f #USDCHF
+            tp1 = self.data.Close[-1] + self.data.ATR[-1]*self.SLTPR/self.atr_f
+            self.buy(sl=sl1, tp=tp1)
+        elif self.signal1==1:
+            #sl1 = self.data.Close[-1] + 750e-4 #EURUSD
+            #tp1 = self.data.Close[-1] - 600e-4
+            #sl1 = self.data.Close[-1] + 30 #ETHUSD
+            #tp1 = self.data.Close[-1] - 40
+            sl1 = self.data.Close[-1] + self.data.ATR[-1]/self.atr_f #USDCHF
+            tp1 = self.data.Close[-1] - self.data.ATR[-1]*self.SLTPR/self.atr_f
+            self.sell(sl=sl1, tp=tp1)
+
+def showma():
+    bt = Backtest(df, MyStrat, cash=10000, commission=0.000)
+    stat = bt.run()
+    st.write('Strategy Returns [%] : ' + str(dict(stat)['Return [%]']))
+    st.write('Buy & Hold Return [%] : ' + str(dict(stat)['Buy & Hold Return [%]']))
+    st.write('Win Rate [%] : ' + str(dict(stat)['Win Rate [%]']))
+
+
 if gobtn or ticker:
-    st.header("MA Cross Strategy")
-    macross()
+    st.header("MA Strategy")
+    showma()
 
 
 st.write("---")
