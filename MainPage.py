eca8378d49552b99cd2cc03f9b00769bb83b0212
Abhishek Bhosale <abhisheksachinbhosale@gmail.com>
2022-07-09 12:08:41 +0530

    changes in ui

--- MainPage.py
+++ MainPage.py
@@ -31,7 +31,7 @@ st.markdown('''
             h1{
             text-align: center;
             font-weight: 600;
-            font-size: 2.2rem;
+            font-size: 2.5rem;
             }
             h3{
             text-align: center;
@@ -47,7 +47,7 @@ st.markdown('''
 st.title(' 📈 ALGO ALERT 📉 ')
 st.markdown('''
             <p style="text-align:center; marginbottom:0px; margin-top:10px;"> 
-            <img src="https://i.postimg.cc/MKxNT9QQ/8432.jpg" class="img-fluid hover-shadow" height="318px" width="512px" alt="Cinque Terre">
+            <img src="https://i.postimg.cc/MKxNT9QQ/8432.jpg" class="img-fluid hover-shadow" height="270px" width="480px" alt="Cinque Terre">
             </p>
             ''', unsafe_allow_html=True)
 st.write('---')
@@ -76,7 +76,7 @@ with c1:
     st.subheader('SRS 1')
     st.markdown('''
             <p style="text-align:center; marginbottom:12px; margin-top:12px;"> 
-            <img src="https://i.postimg.cc/Dw2GGjLS/i1.png" class="img-fluid hover-shadow" height="144px" width="256" alt="Cinque Terre">
+            <img src="https://i.postimg.cc/QtWXmf8b/i3.png" class="img-fluid hover-shadow" height="64px" width="64px" alt="Cinque Terre">
             </p>
             ''', unsafe_allow_html=True)
     st.button('Check Stocks', key='b3')
@@ -84,7 +84,7 @@ with c2:
     st.subheader('SRS 2 ')
     st.markdown('''
             <p style="text-align:center; marginbottom:12px; margin-top:12px;"> 
-            <img src="https://i.postimg.cc/1zFy3PXw/i2.png" class="img-fluid hover-shadow" height="144px" width="256" alt="Cinque Terre">
+            <img src="https://i.postimg.cc/QtWXmf8b/i3.png" class="img-fluid hover-shadow" height="64px" width="64px" alt="Cinque Terre">
             </p>
             ''', unsafe_allow_html=True)
     st.button('Check Stocks', key='b4')
